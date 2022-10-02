a36215ffe87e0501f5d7792aab11aac9aa770a90
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-10-02 18:33:41 +0530

    Added the BMI Calculator App

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.bmicalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bmicalculator.ui.theme.BMICalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BMICalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    mainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun mainScreen() {
+    var height by remember {
+        mutableStateOf("")
+    }
+
+    var weight by remember {
+        mutableStateOf("")
+    }
+
+    var result by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
+        Text(text = "BMI CALCULATOR")
+        TextField(
+            modifier = Modifier.padding(15.dp),
+            value = height,
+            onValueChange = { height = it },
+            label = {
+                Text(text = "Height (M)")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        TextField(
+            modifier = Modifier.padding(15.dp),
+            value = weight,
+            onValueChange = { weight = it },
+            label = {
+                Text(text = "Weight (KG)")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Button(
+            onClick = {
+                      result = calculateBmi(height.toDouble(), weight.toDouble())
+            },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Calculate")
+        }
+
+        if(result.isNotBlank()) {
+            Text(text = result)
+        }
+
+    }
+}
+
+private fun calculateBmi(height: Double, weight: Double): String {
+    val bmiIndex = weight / (height * height)
+    return "Result: $bmiIndex"
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BMICalculatorTheme {
+        mainScreen()
+    }
+}
